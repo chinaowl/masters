c47dae2ac78367d67c3fe157f675dfe86816f1d5
China Wang <chinana523@gmail.com>
2016-03-10 23:51:26 -0800

    added reusable layout component

--- src/router.js
+++ src/router.js
@@ -1,19 +1,32 @@
 import Router from 'ampersand-router';
 import React from 'react';
+import Layout from './layout';
 import HomePage from './pages/home';
 import ReposPage from './pages/repos';
 
 export default Router.extend({
+	renderPage(page, opts = {layout: true}) {
+		if (opts.layout) {
+			page = (
+				<Layout>
+				  {page}
+				</Layout>
+			);
+		}
+		
+		React.render(page, document.body)
+	},
+
 	routes: {
 		'': 'home',
 		'repos': 'repos'
 	},
 
 	home() {
-		React.render(<HomePage />, document.body);
+		this.renderPage(<HomePage/>, {layout: false});
 	},
 
 	repos() {
-		React.render(<ReposPage />, document.body);
+		this.renderPage(<ReposPage/>);
 	}
 });
