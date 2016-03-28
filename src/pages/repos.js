cc5f804626d9a835564ef22a13f64db00478dee7
China Wang <chinana523@gmail.com>
2016-03-27 17:52:57 -0700

    render repos

--- src/pages/repos.js
+++ src/pages/repos.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import ampersandMixin from 'ampersand-react-mixin';
 
 export default React.createClass({
+	mixins: [ampersandMixin],
+	
 	render() {
+		const {repos} = this.props;
+
 		return (
-			<h1>Repos</h1>
+			<div>
+				<h2>Repos</h2>
+				<ul>
+				{repos.map((repo) => {
+					return (<li><a href="">{repo.full_name}</a></li>)
+				})}
+				</ul>
+			</div>
 		);
 	}
 });
